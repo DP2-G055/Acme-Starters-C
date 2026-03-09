b9e94af1388fba4c63d1d28fb32257856345e0f9
kevamacal <kevamacal@alum.us.es>
2026-03-09 10:37:16 +0100

    feat: implementados casos de usos para csvs + comprobacion null en validator

--- src/main/java/acme/constraints/InventionValidator.java
+++ src/main/java/acme/constraints/InventionValidator.java
@@ -34,26 +34,27 @@ public class InventionValidator extends AbstractValidator<ValidInvention, Invent
 			result = true;
 		else {
 			{
-				boolean uniqueInvention;
+				boolean uniqueInvention = false;
 				Invention existingInvention;
-
-				existingInvention = this.repository.findInventionByTicker(invention.getTicker());
-				uniqueInvention = existingInvention == null || existingInvention.equals(invention);
+				if (invention.getTicker() != null) {
+					existingInvention = this.repository.findInventionByTicker(invention.getTicker());
+					uniqueInvention = existingInvention == null || existingInvention.equals(invention);
+				}
 
 				super.state(context, uniqueInvention, "ticker", "acme.validation.invention.duplicated-ticker.message");
 			}
 			{
-				boolean hasParts;
+				boolean hasParts = false;
 				int numParts = this.repository.countPartsByInventionId(invention.getId());
-
-				hasParts = invention.getDraftMode() || numParts > 0;
+				if (invention.getDraftMode() != null)
+					hasParts = invention.getDraftMode() || numParts > 0;
 
 				super.state(context, hasParts, "*", "acme.validation.invention.parts.message");
 			}
 			{
-				boolean isValidInterval;
-
-				isValidInterval = MomentHelper.isAfter(invention.getEndMoment(), invention.getStartMoment());
+				boolean isValidInterval = false;
+				if (invention.getStartMoment() != null && invention.getEndMoment() != null)
+					isValidInterval = MomentHelper.isAfter(invention.getEndMoment(), invention.getStartMoment());
 
 				super.state(context, isValidInterval, "endMoment", "acme.validation.invention.valid-interval.message");
 			}
