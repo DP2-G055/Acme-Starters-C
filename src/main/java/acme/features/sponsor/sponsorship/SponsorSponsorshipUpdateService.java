16575ac0a607d09595fb667a3620a843147e3e5c
Pablobi <pablo.bermudez.medina@gmail.com>
2026-03-16 16:51:47 +0100

    fix: update sponsorship unbinding

--- src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipUpdateService.java
+++ src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipUpdateService.java
@@ -55,7 +55,7 @@ public class SponsorSponsorshipUpdateService extends AbstractService<Sponsor, Sp
 
 	@Override
 	public void unbind() {
-		super.unbindObject(this.sponsorship, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "dratMode");
+		super.unbindObject(this.sponsorship, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "draftMode");
 	}
 
 }
